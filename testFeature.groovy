8629a1ebfeb3e32c64375c2e68fcec6a482eae77
echt06 <bastian.echterhoelter@bertelsmann.de>
2014-08-08 19:59:30 -0700

    Added cucumber feature

---
+++ testFeature.groovy
@@ -0,0 +1,3 @@
+Given a PC9Product with the ID 554280051and a SKU with the ID 554280051036 which has no stock
+When accessing the PDP Page and hovering over the size 36 on the buystack
+Then the add to cart button should not be displayed and the Out of Stock section should be visible instead.
